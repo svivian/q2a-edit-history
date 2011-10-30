f583cd7088b083f5c95953954c412de6415ebff2
Scott Vivian <scott.vivian@gmail.com>
2011-10-30 01:56:13 +0100

    basic half-working plugin

---
+++ qa-edit-history.php
@@ -0,0 +1,89 @@
+<?php
+/*
+	Question2Answer Edit History plugin, v0.1
+	License: http://www.gnu.org/licenses/gpl.html
+*/
+
+require_once QA_INCLUDE_DIR.'qa-app-users.php';
+
+class qa_edit_history
+{
+	private $directory;
+	private $urltoroot;
+
+	private $opt = 'edit_history_active';
+
+	public function load_module($directory, $urltoroot)
+	{
+		$this->directory = $directory;
+		$this->urltoroot = $urltoroot;
+	}
+
+	function admin_form( &$qa_content )
+	{
+		$saved_msg = '';
+
+		if ( qa_clicked('edit_history_activate_button') )
+		{
+			// create table
+			$sql_create =
+				'CREATE TABLE IF NOT EXISTS ^edit_history ( ' .
+				'`postid` int(10) unsigned NOT NULL, ' .
+				'`edited` datetime NOT NULL, ' .
+				'`title` varchar(800) DEFAULT NULL, ' .
+				'`content` varchar(8000) DEFAULT NULL, ' .
+				'`tags` varchar(800) DEFAULT NULL, ' .
+				'`userid` int(10) unsigned DEFAULT NULL, ' .
+				'`reason` varchar(800) DEFAULT NULL, ' .
+				'PRIMARY KEY (`postid`,`edited`) ' .
+				') ENGINE=InnoDB DEFAULT CHARSET=utf8;';
+			$result = qa_db_query_sub($sql_create);
+
+			if ( $result === true )
+			{
+				qa_opt( $this->opt, '1' );
+				$saved_msg = 'Plugin activated!';
+			}
+		}
+
+		if ( !qa_opt($this->opt) )
+		{
+			return array(
+				'buttons' => array(
+					array(
+						'label' => 'Activate Edit History',
+						'tags' => 'name="edit_history_activate_button"',
+						'note' => '(not set up yet)',
+					),
+				),
+			);
+		}
+
+		return array(
+			'ok' => $saved_msg,
+		);
+
+	}
+
+
+	function process_event( $event, $userid, $handle, $cookieid, $params )
+	{
+		// only interested in edits
+		$attachevents = array('q_edit', 'a_edit');
+		if ( !in_array( $event, $attachevents ) )
+			return;
+
+		// get config data
+		if ( !qa_opt($this->opt) )
+			return;
+
+		$userid = qa_get_logged_in_userid();
+
+		$sql =
+			'INSERT INTO ^edit_history (postid, edited, title, content, tags, userid, reason) ' .
+			'VALUES (#, NOW(), $, $, $, #, $)';
+
+		return qa_db_query_sub( $sql, $params['postid'], $params['oldtitle'], $params['oldcontent'], $params['oldtags'], $userid, '' );
+	}
+
+}
---
+++ qa-plugin.php
@@ -0,0 +1,33 @@
+<?php
+/*
+	Plugin Name: Edit History
+	Plugin URI: https://github.com/svivian/q2a-edit-history
+	Plugin Description: Edit History plugin for Q2A
+	Plugin Version: 0.1
+	Plugin Date: 2011-10-29
+	Plugin Author: Scott Vivian
+	Plugin Author URI: http://codelair.co.uk/
+	Plugin License: GPLv3
+	Plugin Minimum Question2Answer Version: 1.4
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.gnu.org/licenses/gpl.html
+*/
+
+if ( !defined('QA_VERSION') )
+{
+	header('Location: ../../');
+	exit;
+}
+
+
+qa_register_plugin_module('event', 'qa-edit-history.php', 'qa_edit_history', 'Edit History');
