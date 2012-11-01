3e0c6869de90d1e64daed1ec403bc9121483052c
Scott Vivian <scott.vivian@gmail.com>
2012-11-01 17:04:54 +0000

    only show revisions link when they exist, also language independent

--- qa-edh-layer.php
+++ qa-edh-layer.php
@@ -6,12 +6,42 @@
 
 class qa_html_theme_layer extends qa_html_theme_base
 {
+	private $edhist_key = 'edit_history';
+	private $rev_postids = array();
+
+	function doctype()
+	{
+		if ( $this->template == 'question' )
+		{
+			// grab a list of all posts on this page
+			$postids = array();
+			$postids[] = $this->content['q_view']['raw']['postid'];
+			foreach ( $this->content['q_view']['c_list']['cs'] as $comm )
+				$postids[] = $comm['raw']['postid'];
+
+			foreach ( $this->content['a_list']['as'] as $answ )
+			{
+				$postids[] = $answ['raw']['postid'];
+				foreach ( $answ['c_list']['cs'] as $comm )
+					$postids[] = $comm['raw']['postid'];
+			}
+
+			// check for revisions in these posts
+			$sql = 'SELECT DISTINCT postid FROM ^' . $this->edhist_key .
+				' WHERE postid IN (' . implode(',', $postids) . ') GROUP BY postid';
+			$this->rev_postids = qa_db_read_all_values( qa_db_query_sub($sql) );
+		}
+
+		parent::doctype();
+	}
+
 	function post_meta($post, $class, $prefix=null, $separator='<BR/>')
 	{
-		if ( @$post['what_2'] == 'edited' )
+		// only link when there are actual revisions
+		if ( in_array( $post['raw']['postid'], $this->rev_postids ) )
 		{
 			$url = qa_path_to_root() . 'revisions/' . $post['raw']['postid'];
-			$post['what_2'] = '<a rel="nofollow" href="'.$url.'" class="'.$class.'-revised">'.$post['what_2'].'</a>';
+			$post['when_2']['data'] = '<a rel="nofollow" href="'.$url.'" class="'.$class.'-revised">' . $post['when_2']['data'] . '</a>';
 		}
 
 		parent::post_meta($post, $class, $prefix, $separator);
